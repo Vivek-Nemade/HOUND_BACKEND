db6b3d7bd48c66e05f7f19a186602f08edfd532c
Vivek Nemade <viveknemade11@gmail.com>
2024-02-10 15:12:16 +0530

    change password controller and route added

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,7 +1,7 @@
 
 import express from "express"
 import {upload} from "../middlewares/multer.middleware.js"
-import { getUser, getUserByParams, getUserLikesAndCommentsCount, loginUser, logoutUser, registerUser, updateUserData, uploadUserimages } from "../controllers/user.controller.js";
+import { getUser, getUserByParams, getUserLikesAndCommentsCount, loginUser, logoutUser, registerUser, updatePassword, updateUserData, uploadUserimages } from "../controllers/user.controller.js";
 import { verifyJwt } from "../middlewares/auth.middelware.js";
 import { Likedblogs } from "../controllers/like.controller.js";
 import { addFollow, getFollowStatus, getFollowersAndFollowing, removeFollower } from "../controllers/subscribe.controller.js";
@@ -45,7 +45,7 @@ router.route('/userdata/:userId').get(verifyJwt,getUserByParams)
 router.route('/userdata/:userId/followdata').get(verifyJwt,getFollowersAndFollowing)
 router.route('/current-user/liked-Blogs').get(verifyJwt,Likedblogs)
 router.route('/current-user/likesAndComments').get(verifyJwt,getUserLikesAndCommentsCount)
-
+router.route('/current-user/change-password').post(verifyJwt,updatePassword)
 
         export default router
 
