520c51fa2eec37023c2fb7b0741574351888a046
Vivek Nemade <viveknemade11@gmail.com>
2024-02-20 20:10:21 +0530

    added cors origin https://hound-frontend-service.vercel.app

--- server/src/server.js
+++ server/src/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: 'https://hound-frontend-service.vercel.app',
 }));
 // app.use(cors())
 app.use(express.json());
